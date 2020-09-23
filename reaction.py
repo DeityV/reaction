c414daefc748e5d9c7e61e525ce6a9a88afea315
Diego Vester <vestergee3@gmail.com>
2020-09-23 17:27:19 -0500

    Add files via upload

---
+++ reaction.py
@@ -0,0 +1,53 @@
+import discord
+
+client = discord.Client()
+@client.event
+async def on_raw_reaction_add(payload):
+    message_id = payload.essage_id
+    if message_id == #############:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+        if payload.emoji.name == '###':
+            role = discord.utils.get(guild.roles, name = '###')
+        elif payload.emoji.name == '###':
+            role = discord.utils.get(guild.roles, name='###')
+        else:
+            role = discord.utils.get(guild.roles, name = payload.emoji.name)
+        
+        if role is not None:
+            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+            if member is not None:
+                await member.add_roles(role)
+                print("done")
+            else:
+                print("Member not found.")
+        
+        else:
+            print("Role not found.")
+    
+@client.event
+async def on_raw_reaction_remove(payload):
+    message_id = payload.message_id
+    if message_id == ###:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+        if payload.emoji.name == '###':
+            role = discord.utils.get(guild.roles, name='###')
+        elif payload.emoji.name == '###':
+            role = discord.utils.get(guild.roles, name='###')
+        else:
+            role = discord.utils.get(guild.roles, name = payload.emoji.name)
+        
+        if role is not None:
+            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+            if member is not None:
+                await member.remove_roles(role)
+                print("done")
+            else:
+                print("Member not found.")
+        else:
+            print("Role not found.")
+
+client.run('###')
